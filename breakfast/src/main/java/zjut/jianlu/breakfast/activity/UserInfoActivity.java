79cd94b4180bd59051cd8f7d285eb6684ca6c4e9
jianlu <815093244@qq.com>
2016-03-11 10:36:57 +0800

    注册登录

--- breakfast/src/main/java/zjut/jianlu/breakfast/activity/UserInfoActivity.java
+++ breakfast/src/main/java/zjut/jianlu/breakfast/activity/UserInfoActivity.java
@@ -18,10 +18,6 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.Poi;
-
-
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -49,6 +45,7 @@ public class UserInfoActivity extends BaseActivity {
     private String userName;
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new MyLocationListener();
+    private String mAddress;
 
 
     @Bind(R.id.iv_back)
@@ -69,6 +66,8 @@ public class UserInfoActivity extends BaseActivity {
     Button btnNext;
     @Bind(R.id.address_Linear)
     LinearLayout mLlAddress;
+    @Bind(R.id.iv_location)
+    ImageView mLv_location;
 
 
     @Override
@@ -76,9 +75,9 @@ public class UserInfoActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
         ButterKnife.bind(this);
-        mLocationClient = new LocationClient(getApplicationContext());
-        mLocationClient.registerLocationListener(myListener);
-        initLoctaion();
+//        mLocationClient = new LocationClient(getApplicationContext());
+
+
         genders = getResources().getStringArray(R.array.gender);
         types = getResources().getStringArray(R.array.type);
         initView();
@@ -87,6 +86,7 @@ public class UserInfoActivity extends BaseActivity {
             mobile = intent.getStringExtra(BreakfastConstant.MOBILE_TAG);
             password = intent.getStringExtra(BreakfastConstant.PASSWORD_TAG);
         }
+//        mLocationClient.start();
     }
 
     private void initLoctaion() {
@@ -94,14 +94,7 @@ public class UserInfoActivity extends BaseActivity {
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span = 1000;
-        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
-        option.setOpenGps(true);//可选，默认false,设置是否使用gps
-        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
         option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
         option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
         mLocationClient.setLocOption(option);
@@ -139,7 +132,7 @@ public class UserInfoActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.sex_input, R.id.gender_ImageButton, R.id.type_input, R.id.type_ImageButton, R.id.btn_next})
+    @OnClick({R.id.sex_input, R.id.gender_ImageButton, R.id.type_input, R.id.type_ImageButton, R.id.btn_next, R.id.iv_location})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_back:
@@ -157,6 +150,17 @@ public class UserInfoActivity extends BaseActivity {
             case R.id.btn_next:
                 checkInput();
 //                showToast("点击下一个按钮");
+                break;
+            case R.id.iv_location:
+//                mLocationClient.registerLocationListener(myListener);
+//                initLoctaion();
+//                if (!mLocationClient.isStarted()) {
+//                    mLocationClient.start();
+//                }
+//                if (!TextUtils.isEmpty(mAddress)) {
+//
+//                    etAddress.setText(mAddress);
+//                }
                 break;
         }
     }
@@ -225,54 +229,26 @@ public class UserInfoActivity extends BaseActivity {
             sb.append(location.getLongitude());
             sb.append("\nradius : ");
             sb.append(location.getRadius());
-            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-                sb.append("\nspeed : ");
-                sb.append(location.getSpeed());// 单位：公里每小时
-                sb.append("\nsatellite : ");
-                sb.append(location.getSatelliteNumber());
-                sb.append("\nheight : ");
-                sb.append(location.getAltitude());// 单位：米
-                sb.append("\ndirection : ");
-                sb.append(location.getDirection());// 单位度
+            if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
                 sb.append("\naddr : ");
-                sb.append(location.getAddrStr());
-                sb.append("\ndescribe : ");
-                sb.append("gps定位成功");
-
-            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-                sb.append("\naddr : ");
+                mAddress = location.getAddrStr();
                 sb.append(location.getAddrStr());
                 //运营商信息
                 sb.append("\noperationers : ");
                 sb.append(location.getOperators());
                 sb.append("\ndescribe : ");
                 sb.append("网络定位成功");
-            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                sb.append("\ndescribe : ");
-                sb.append("离线定位成功，离线定位结果也是有效的");
-            } else if (location.getLocType() == BDLocation.TypeServerError) {
-                sb.append("\ndescribe : ");
-                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                sb.append("\ndescribe : ");
-                sb.append("网络不同导致定位失败，请检查网络是否通畅");
-            } else if (location.getLocType()     == BDLocation.TypeCriteriaException) {
-                sb.append("\ndescribe : ");
-                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            } else {
+                showToast("定位失败请先打开定位");
             }
-            sb.append("\nlocationdescribe : ");
-            sb.append(location.getLocationDescribe());// 位置语义化信息
-            List<Poi> list = location.getPoiList();// POI数据
-            if (list != null) {
-                sb.append("\npoilist size = : ");
-                sb.append(list.size());
-                for (Poi p : list) {
-                    sb.append("\npoi= : ");
-                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-                }
-            }
+
             Log.i("BaiduLocationApiDem", sb.toString());
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//       mLocationClient.stop();
+    }
 }
